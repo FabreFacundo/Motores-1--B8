a4e1b09c562e6d452da6155aea08cfc9140bc74d
Facundo Fabre <facufabre2011@gmail.com>
2025-08-31 04:26:08 -0300

    deteccion y ataque

--- Assets/Enemy_agent.cs
+++ Assets/Enemy_agent.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,17 +9,28 @@ using UnityEngine.UIElements;
 public class Enemy_agent : MonoBehaviour
 {
     public enum ENEMY_STATE { PATROLLING, INVESTIGATING, SEARCHING, ATTACKING }
+    public enum ENEMY_PATROL_TYPE { ONE_WAY, FORWARD_AND_BACK, CIRCULAR }
     #region INSPECTOR_ATTRIBUTES
-    [Header("Patrol asignment")]
+
+  
+
+    [Header("Patrol attributes")]
     [SerializeField] private WaypointPath _patrolSector;
+    [SerializeField] private ENEMY_PATROL_TYPE _patrolType;
+
     [Header("NavMeshAgent attributes")]
     [SerializeField][Range(0.1f, 5f)] private float _stoppingDistance = 0.5f;
     [SerializeField][Range(0.1f, 50f)] private float _patrolSpeed = 15f;
     [SerializeField][Range(0.1f, 50f)] private float _searchingSpeed = 10f;
     [SerializeField][Range(0.1f, 50f)] private float _attackingSpeed = 20f;
     [SerializeField][Range(0.1f, 50f)] private float _aceleration = 5f;
-    [Header("Searching & investigating attributes")]
-    [SerializeField][Range(0.1f, 50f)] private float _secBetweenTurn = 5;
+
+    [Header("Attack attributes")]
+    [SerializeField][Range(0.1f, 50f)] private float _minimalAttacKDistance;
+
+    [Header("Searching attributes")]
+    [SerializeField][Range(0.1f, 50f)] private float _searchingTime = 5;
+
     [Header("GUI attributes")]
     [SerializeField] private Color _stoppingDistanceVisualization = Color.red;
     #endregion
@@ -29,9 +41,15 @@ public class Enemy_agent : MonoBehaviour
     private NavMeshAgent _agent;
     private Vector3 _lastPlayerPosition;
     private bool _onSearch;
-    private bool _haveReach;
+    private bool _invertPatrol;
+    private float _elapsedTime=0;
+    private Transform _player;
     #endregion
     #region PROPERTIES
+    public bool OnSearch
+    {
+        get { return _onSearch; }
+    }
     public Vector3 LastPlayerPosition
     {
         get { return _lastPlayerPosition;}
@@ -48,31 +66,56 @@ public class Enemy_agent : MonoBehaviour
             if (value != _actualState) _actualState = value;
         }
     }
+    public NavMeshAgent Agent
+    {
+        get { return _agent; }
+    }
+    public Transform Player
+    {
+        get { return _player; }
+    }
     #endregion
 
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _agent.acceleration = _aceleration;
-        _agent.stoppingDistance = _stoppingDistance;  
+        _agent.stoppingDistance = _stoppingDistance;
+        _player = GameObject.Find("Player").transform;
     }
-    // Update is called once per frame
     void Update()
     {
-     
-        switch(_actualState)
+      
+        switch (_actualState)
         {
             case ENEMY_STATE.PATROLLING:
-                 Patrol(_patrolSector);
+                 Patrol();
                 break;
             case ENEMY_STATE.INVESTIGATING:
                 Investigate(_lastPlayerPosition);
+                break;
+            case ENEMY_STATE.SEARCHING:
+                Search();
+                break;
+            case ENEMY_STATE.ATTACKING:
+                Attack();
                 break;
         }
        
     }
 
-  
+    private void Attack()
+    {
+        if (_player == null) return;
+        _agent.speed = _attackingSpeed;
+        _agent.destination = _player.position;
+        if (_agent.remainingDistance < _minimalAttacKDistance)
+        {
+            _agent.isStopped = true;
+            Debug.Log("Atacando al jugador");
+        }
+        else _agent.isStopped = false;
+    }
     private void Investigate(Vector3 playerPosition)
     {
         if (!_onSearch)
@@ -85,46 +128,95 @@ public class Enemy_agent : MonoBehaviour
         if (!_agent.pathPending && _agent.remainingDistance <= _stoppingDistance)
         {
             _actualState = ENEMY_STATE.SEARCHING;
-            StartCoroutine(Search());
-
         }
 
 
     }
-    IEnumerator Search()
+    private void Search()
     {
-        transform.Rotate(0, 90, 0);
-        yield return new WaitForSecondsRealtime(_secBetweenTurn);
-        transform.Rotate(0, -180, 0);
-        yield return new WaitForSecondsRealtime(_secBetweenTurn);
-        _actualState = ENEMY_STATE.PATROLLING;
-        _onSearch = false;
+        _elapsedTime += Time.deltaTime;
+        if(_elapsedTime >= _searchingTime)
+        {
+            _actualState = ENEMY_STATE.PATROLLING;
+            _onSearch = false;
+        }
     }
-    private void Patrol(WaypointPath sector)
+    private void Patrol()
     {
-        if(sector == null) return;
-        if (sector.Waypoints.Count == 0) return;
-       
+
+        if(_patrolSector == null) return;
+        if (_patrolSector.Waypoints.Count == 0) return;
         if (!_agent.hasPath)
         {
-            _startWaypoint = sector.Waypoints[0];
+            _startWaypoint = _patrolSector.Waypoints[0];
             _agent.speed = _patrolSpeed;
             _agent.SetDestination(_startWaypoint);
         }
         else
         {
-
-            if (!_agent.pathPending && _agent.remainingDistance<=_stoppingDistance)
+            switch (_patrolType)
             {
-                if (_nextWaypoint == sector.Waypoints.Count) _nextWaypoint = 0;
-                _agent.destination = sector.Waypoints[_nextWaypoint];
+                case ENEMY_PATROL_TYPE.ONE_WAY:
+                    OneWayPatrol();
+                    break;
+                case ENEMY_PATROL_TYPE.CIRCULAR:
+                    CircularPatrol();
+                    break;
+                case ENEMY_PATROL_TYPE.FORWARD_AND_BACK:
+                    ForwardBackPatrol();
+                    break;
+            }
+        }
+            
+
+    }
+
+   private void CircularPatrol()
+    {
+
+            if (!_agent.pathPending && _agent.remainingDistance <= _stoppingDistance)
+            {
+                if (_nextWaypoint == _patrolSector.Waypoints.Count) _nextWaypoint = 0;
+                _agent.destination = _patrolSector.Waypoints[_nextWaypoint];
                 _nextWaypoint++;
 
             }
-                
+    }
+
+    private void OneWayPatrol()
+    {
+        if (!_agent.pathPending && _agent.remainingDistance <= _stoppingDistance)
+        {
+            if (_nextWaypoint == _patrolSector.Waypoints.Count)
+            {
+                _actualState = ENEMY_STATE.SEARCHING;
+                return;
+            }
+            _agent.destination = _patrolSector.Waypoints[_nextWaypoint];
+            _nextWaypoint++;
 
         }
     }
+
+    private void ForwardBackPatrol()
+    {
+
+        if (!_agent.pathPending && _agent.remainingDistance <= _stoppingDistance)
+        {
+            if (_nextWaypoint == _patrolSector.Waypoints.Count - 1 || _nextWaypoint == 0)
+            {
+                _invertPatrol = !_invertPatrol;
+            }
+   
+            _agent.destination = _patrolSector.Waypoints[_nextWaypoint];
+            if(_invertPatrol)
+                _nextWaypoint--;
+            else
+                _nextWaypoint++;
+
+        }
+    }
+
 
     #region ON_SCENE_VISUALIZATION
     private void OnDrawGizmos()
@@ -139,6 +231,16 @@ public class Enemy_agent : MonoBehaviour
             case ENEMY_STATE.INVESTIGATING:
                 Gizmos.color = Color.yellow;
                 Gizmos.DrawSphere((transform.position + new Vector3(0, transform.localScale.y , 0)), 0.5f);
+                Gizmos.color = Color.white;
+                break;
+            case ENEMY_STATE.SEARCHING:
+                Gizmos.color = Color.blue;
+                Gizmos.DrawSphere((transform.position + new Vector3(0, transform.localScale.y, 0)), 0.5f);
+                Gizmos.color = Color.white;
+                break;
+            case ENEMY_STATE.ATTACKING:
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere((transform.position + new Vector3(0, transform.localScale.y, 0)), 0.5f);
                 Gizmos.color = Color.white;
                 break;
         }
--- Assets/Survilance.cs
+++ Assets/Survilance.cs
@@ -1,19 +1,101 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Survilance : MonoBehaviour
 {
-
+    #region INSPECTOR_ATTRIBUTES
+    [SerializeField] private float _maxDetectionTime = 3f;
+    [SerializeField][Range(0.01f,1f)]private float _cooldawnFactor = 0.2f;
+    [SerializeField] private LayerMask _playerLayer;
+    [SerializeField] private float _coneRotationSpeed;
+    [SerializeField] private float _maxAngle;
+    [SerializeField] private float _minimalDetectionDistance = 5;
+    [SerializeField] private Transform _parent;
+    #endregion
     #region INTERNAL_ATTRIBUTES
+ 
     private Enemy_agent _agent;
+    private Transform _player;
+    private Vector3 _playerDirection;
+    private float _detectionTime;
+    private float _playerAngle;
+    private bool _playerInCone;
+    private RaycastHit hit;
+    private bool _playerInSight;
     #endregion
     private void Start()
     {
-        _agent = GetComponent<Enemy_agent>();   
+        _agent = GetComponentInParent<Enemy_agent>();
+        _player = _agent.Player;
+        
+    }
+     private void Update()
+    {
+        Debug.Log(_detectionTime + " " + _agent.Agent.remainingDistance);
+        if (_agent.OnSearch && (_detectionTime > _maxDetectionTime || _agent.Agent.remainingDistance < _minimalDetectionDistance))
+        {
+            SightDetected();
+        }
+        if(!_playerInSight && _detectionTime > 0 )
+        {
+            if (_detectionTime < 0)
+            {
+                _detectionTime = 0;
+                CallToInvestigate(_agent.LastPlayerPosition);
+            }
+            _detectionTime -= Time.deltaTime * _cooldawnFactor;
+        }
+        if(!_playerInSight)
+        {
+            _coneRotationSpeed = Mathf.Abs(_parent.localRotation.eulerAngles.y) > _maxAngle ? _coneRotationSpeed * -1 : _coneRotationSpeed * 1;
+            _parent.Rotate(new Vector3(0, _coneRotationSpeed * Time.deltaTime, 0));
+        }
+        else
+        {
+            _parent.LookAt(_player);
+        }
+        
+
+    }
+    public void NoiseDetected(Vector3 position)
+    {
+        CallToInvestigate(position);
+    }
+    private void SightDetected()
+    {
+        _agent.ActualState = Enemy_agent.ENEMY_STATE.ATTACKING;
     }
 
-    public void NoiseDetected(Vector3 Position)
+    private void CallToInvestigate(Vector3 position)
     {
-        _agent.LastPlayerPosition = Position; 
+        _agent.LastPlayerPosition = position;
         _agent.ActualState = Enemy_agent.ENEMY_STATE.INVESTIGATING;
+    }
+
+     void OnTriggerEnter(Collider collision)
+    {
+    
+        if (1 << collision.gameObject.layer == _playerLayer)
+        {
+       
+            CallToInvestigate(collision.gameObject.transform.position);
+            _playerInSight = true;
+        }
+    }
+     void OnTriggerStay(Collider collision)
+    {
+        if(1<<collision.gameObject.layer == _playerLayer )
+        {
+            //AGREGAR raycast
+            _detectionTime += Time.fixedDeltaTime;
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (1 << collision.gameObject.layer == _playerLayer)
+        {
+            _agent.LastPlayerPosition = _player.position;
+            _playerInSight = false;
+        } 
     }
 }
